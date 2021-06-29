b690c99ef9e3959b05f2fc9b60850642f40b133e
Arnold Khovrashkov <j4st.j4h@gmail.com>
2021-06-29 09:30:00 +0300

    Add files via upload

--- src/com/company/InteriorParser.java
+++ src/com/company/InteriorParser.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 public class InteriorParser {
 
-public List <User> userArrayList;
-public List <String> parsedUserList;
-public List <String> uniqueUsernameList;
+private List <User> userArrayList;
+private List <String> uniqueUserLeavedComment;
 
 
 public List<User> parseUsernames () throws IOException {
@@ -16,22 +15,27 @@ public List<User> parseUsernames () throws IOException {
     FileParser fileParser = new FileParser();
     userArrayList = fileParser.parseFromFile(fileParser.getFileSushi());
 
-    parsedUserList = new ArrayList<>();
-    uniqueUsernameList = new ArrayList<>();
+    uniqueUserLeavedComment = new ArrayList<>();
 
 
     for ( int i = 0; i < userArrayList.size() - 1; i++ ) {
 
         String comment = userArrayList.get(i).getComment();
-        int id = userArrayList.get(i).getId();
 
-        for (int j = i+1; j<userArrayList.size() - 1; j++) {
-            if (comment.contains(userArrayList.get(j).getUsername()) && id < userArrayList.get(j).getId()) {
+        for (int j = i+1; j<userArrayList.size(); j++) {
+            if (comment.contains(userArrayList.get(j).getUsername())) {
 
-                parsedUserList.add(userArrayList.get(j).getUsername());
+                List<User> sublistUsers = userArrayList.subList(0, userArrayList.indexOf(userArrayList.get(j)));
+                List<String> sublistUsernames = new ArrayList<>();
+                for (User user : sublistUsers) {
+                    sublistUsernames.add(user.getUsername());
+                }
+
+                if (! sublistUsernames.contains(userArrayList.get(j).getUsername())) {
+                    uniqueUserLeavedComment.add(userArrayList.get(j).getUsername());
+                }
             }
         }
-
 
     }
 
@@ -40,17 +44,36 @@ public List<User> parseUsernames () throws IOException {
     return userArrayList;
 }
 
+    public List<User> parseTaggedUsernamesComments() {
 
-    public List<String> parseUniqueUsernamesLeavedAComment () {
+        List<User> taggedUsersComments = new ArrayList<>();
 
-    List<String> usersLeavedComment = new ArrayList<>();
+        for ( String aParsedUserList : uniqueUserLeavedComment ) {
 
-        for ( String aParsedUserList : parsedUserList ) {
+            for (User user : userArrayList) {
+                if (user.getUsername().equals(aParsedUserList)) {
+                    taggedUsersComments.add(user);
+                }
+            }
+        }
+
+        return taggedUsersComments;
+    }
+
+
+    public List<String> parseUniqueUsernamesLeavedComment() {
+
+        List<String> usersLeavedComment = new ArrayList<>();
+
+        for ( String aParsedUserList : uniqueUserLeavedComment ) {
+
             if (!usersLeavedComment.contains(aParsedUserList)) {
 
                 usersLeavedComment.add(aParsedUserList);
             }
         }
+
+        System.out.println(usersLeavedComment);
 
         return usersLeavedComment;
     }
@@ -77,13 +100,14 @@ public List<User> parseUsernames () throws IOException {
     public void printInfo () {
 
     List<String> uniqueUsersList = parseUniqueUsernamesFullList();
-    List<String> uniqueUsersLeavedComment = parseUniqueUsernamesLeavedAComment();
+    List<String> uniqueUsersLeavedComment = parseUniqueUsernamesLeavedComment();
+    List<User> taggedUsernamesComments = parseTaggedUsernamesComments();
 
     float uniqueCommentPercent = ((float)uniqueUsersLeavedComment.size()/(float)uniqueUsersList.size()) * 100;
-    float taggedUserCommentsPercent = (float)parsedUserList.size() / (float)userArrayList.size() * 100;
+    float taggedUserCommentsPercent = (float)taggedUsernamesComments.size() / (float)userArrayList.size() * 100;
 
     System.out.println("Всего написано: " + userArrayList.size() + " комментариев");
-    System.out.println("Из них написанных после тэга в комменте: " + parsedUserList.size() + " комментариев, что составляет " +
+    System.out.println("Из них написанных после тэга в комменте: " + taggedUsernamesComments.size() + " комментариев, что составляет " +
         taggedUserCommentsPercent + "% от общего количества комментариев "    );
     System.out.println("Всего уникальных участников: " + uniqueUsersList.size() + " человек");
     System.out.println("Из которых пришло по тэгу в комменте: " + uniqueUsersLeavedComment.size() + " человек, что составляет " +
